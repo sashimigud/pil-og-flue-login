0d467207dc85a04f1f6b4bae6480c7dcce646d59
TrymH <trym.helle@bouvet.no>
2019-10-01 12:59:17 +0200

    Updates

---
+++ precache-manifest.2b5a6db0d261a9457578df33f1cea026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c796cf606428f903bf6ea0b67ba6ab",
+    "url": "/pil-og-flue-login/index.html"
+  },
+  {
+    "revision": "06dd56bbe2df05efd135",
+    "url": "/pil-og-flue-login/static/css/main.1ce82791.chunk.css"
+  },
+  {
+    "revision": "9805112a4015c6b8c13f",
+    "url": "/pil-og-flue-login/static/js/2.7bad4ed8.chunk.js"
+  },
+  {
+    "revision": "06dd56bbe2df05efd135",
+    "url": "/pil-og-flue-login/static/js/main.cf2cfaaf.chunk.js"
+  },
+  {
+    "revision": "685752f2c31f012d055d",
+    "url": "/pil-og-flue-login/static/js/runtime-main.897eae90.js"
+  },
+  {
+    "revision": "5fb48959bbd998f3cdaf0a2237fba647",
+    "url": "/pil-og-flue-login/static/media/Bouvet-logo.5fb48959.png"
+  }
+]);
